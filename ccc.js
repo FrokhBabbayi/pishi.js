8f1e1b784fec320ca49bbf86c447573addfd8023
FrokhBabbayi <138642173+FrokhBabbayi@users.noreply.github.com>
2024-01-08 05:57:17 +0800

    Update ccc.js

--- ccc.js
+++ ccc.js
@@ -3,7 +3,7 @@ const Blogs = () => {
 };
 
 function Car(props) {
-  return <h1>I am a {props.colour} Car!</h1>;
+  return <h1>I am a {props.colouoor} Car!</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
