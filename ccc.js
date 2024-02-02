940b118be6b54a0ec749b4d048f30e8050335cbc
FrokhBabbayi <138642173+FrokhBabbayi@users.noreply.github.com>
2024-02-02 21:58:31 +0800

    Update ccc.js

--- ccc.js
+++ ccc.js
@@ -3,7 +3,7 @@ const Blogs = () => {
 };
 
 function Car(props) {
-  return <h2>I am a {pror} Car!</h2>;
+  return <h2>I am a {pror} Ca!</h2>;
 }
 
 const root = ReactDOM.createRoot(document.getElemeyd('root'));
