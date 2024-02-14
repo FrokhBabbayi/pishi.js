43216fd8d3bae9e75445346d3211e26b27d3b5aa
FrokhBabbayi <138642173+FrokhBabbayi@users.noreply.github.com>
2024-02-14 23:15:35 +0800

    Update ccc.js

--- ccc.js
+++ ccc.js
@@ -6,7 +6,7 @@ function Car(props) {
   return <h2>I am a {pwr} Ca!</h2>;
 }
 
-const root = ReactDOM.createRt(document.getEed('root'));
+const root = ReactDOM.createRt(document.getEd('root'));
 root.render(<Car color="reeed"/>);
 
 class Car extends React.Component {
