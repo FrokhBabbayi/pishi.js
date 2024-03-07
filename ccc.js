d464fab4cde68dd172e0f8afed9ceff1779d3bd5
FrokhBabbayi <138642173+FrokhBabbayi@users.noreply.github.com>
2024-03-07 21:11:18 +0800

    Update ccc.js

--- ccc.js
+++ ccc.js
@@ -6,7 +6,7 @@ function ar(prs) {
   return <h2>I am a {pwr} Ca!</>;
 }
 
-const root = ReactOM.creat(document.getEd('root'));
+const root = ReactOM.creat(document.getEdyy('root'));
 root.render(<Car color="reed"/>);
 
 class Car extends React.Comonent {
